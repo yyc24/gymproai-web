261d951b1548f9912fbf201103ca153e831b8ca1
Yunchao Yang <yunchaoyang31@gmail.com>
2025-08-08 18:57:55 +0900

    fix: split EditMember & NewFeedback into separate files

---
+++ app/members/[id]/feedback/new/page.tsx
@@ -0,0 +1,7 @@
+export default function NewFeedback() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-2xl">反馈表单开发中…</h1>
+    </main>
+  )
+}
